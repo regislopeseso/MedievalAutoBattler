2f0ac564d3ef8b7b210d96ca178cc8b53143ba26
Regis <regis.lopes.eso@gmail.com>
2025-01-28 14:40:42 -0300

    Project configured and ready to start!

---
+++ ProjectThreeAPI/Program.cs
@@ -0,0 +1,30 @@
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddOpenApi();
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseMySql(
+        connectionString,
+        ServerVersion.AutoDetect(connectionString),
+        options => { options.CommandTimeout(120); }
+        );
+});
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
